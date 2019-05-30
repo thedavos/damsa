fc7cb39fe2b740ed9a6f52d56b432116b72d35ab
davos <david@davosdo.com>
2019-05-30 17:25:37 -0500

    iniciando hilos

--- src/app/App.java
+++ src/app/App.java
@@ -9,23 +9,9 @@ import utils.RepositoryImage;
 public class App {
 	public static void main(String[] args) throws Exception {
 		
-		Admin admin = new Admin(
-				74736630,
-				"misael123",
-				"Misael",
-				"Cano Varas",
-				'M',
-				17,
-				"misael@gmail.com",
-				"",
-				123456,
-				997939992,
-				""
-				);
-		
 		AdminModel am = new AdminModel();
 		
-		am.createAdmin(admin);
-		System.out.println(admin);
+		Admin admin = am.getAdmin(75454144);
+		System.out.println(admin.getName());
     }
 }
--- src/models/AdminModel.java
+++ src/models/AdminModel.java
@@ -3,7 +3,9 @@ package models;
 //Java packages
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import clases.Admin;
 import utils.ConnectionDB;
@@ -59,5 +61,125 @@ public class AdminModel {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public Admin getAdmin(int dni) {
+		String query = "";
+		Admin admin = null;
+		
+		try {
+			query = "SELECT * FROM " + tableName + " WHERE "
+					+ "dni = '" + dni + "' AND "
+					+ "estado = 1";
+			
+			Statement statement = this.connect().createStatement();
+			ResultSet result = statement.executeQuery(query);
+			
+			while(result.next()) {
+				
+				int id = result.getInt("admin_id");
+				String code = result.getString("codigo");
+				int dnii = result.getInt("dni");
+				String name = result.getString("nombre");
+				String lastname = result.getString("apellidos");
+				int age = result.getInt("edad");
+				char gender = result.getString("genero").charAt(0);
+				String address = result.getString("direccion");
+				String email = result.getString("correo");
+				int phone = result.getInt("telefono");
+				int cellPhone = result.getInt("celular");
+				String password = result.getString("contraseña");
+				String profileUrl = result.getString("profile_url");
+				
+				admin = new Admin(
+						dnii, 
+						password,
+						name,
+						lastname,
+						gender,
+						age,
+						email,
+						address,
+						phone,
+						cellPhone, 
+						profileUrl
+				);
+				
+				admin.setId(id);
+				admin.setCode(code);
+				admin.setProfileUrl(profileUrl);
+				
+				return admin;
+			}
+			
+			statement.close();
+			this.conn.close();
+			this.conn = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return admin;
+	}
+	
+	public Admin getAdmin(String cod) {
+		String query = "";
+		Admin admin = null;
+		
+		try {
+			query = "SELECT * FROM " + tableName + " WHERE "
+					+ "codigo = '" + cod + "' AND "
+					+ "estado = 1";
+			
+			Statement statement = this.connect().createStatement();
+			ResultSet result = statement.executeQuery(query);
+			
+			while(result.next()) {
+				
+				int id = result.getInt("admin_id");
+				String code = result.getString("codigo");
+				int dnii = result.getInt("dni");
+				String name = result.getString("nombre");
+				String lastname = result.getString("apellidos");
+				int age = result.getInt("edad");
+				char gender = result.getString("genero").charAt(0);
+				String address = result.getString("direccion");
+				String email = result.getString("correo");
+				int phone = result.getInt("telefono");
+				int cellPhone = result.getInt("celular");
+				String password = result.getString("contraseña");
+				String profileUrl = result.getString("profile_url");
+				
+				admin = new Admin(
+						dnii, 
+						password,
+						name,
+						lastname,
+						gender,
+						age,
+						email,
+						address,
+						phone,
+						cellPhone, 
+						profileUrl
+				);
+				
+				admin.setId(id);
+				admin.setCode(code);
+				admin.setProfileUrl(profileUrl);
+				
+				return admin;
+			}
+			
+			statement.close();
+			this.conn.close();
+			this.conn = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return admin;
 	}
 }
