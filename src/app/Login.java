525365c02851d56eb690d12d430994377694ef30
snayder29 <snayder_bryan_29@hotmail.com>
2019-06-29 12:28:14 -0500

    Update Login.java
    actualizacion2

--- src/app/Login.java
+++ src/app/Login.java
@@ -7,6 +7,9 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import org.w3c.dom.css.Counter;
+
 import javax.swing.JLabel;
 
 import javax.swing.JMenu;
@@ -29,6 +32,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import utils.Timer;
 import utils.Validation;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Label;
+import javax.swing.ImageIcon;
 
 public class Login extends JDialog {
 
@@ -41,15 +48,18 @@ public class Login extends JDialog {
 	
 	static public Login dialog = new Login();
 	static public JLabel lblCounter;
+	
+	
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			Login dialog = new Login();
 			dialog.setVisible(true);
 			initThread();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -59,137 +69,107 @@ public class Login extends JDialog {
 	 * Create the dialog.
 	 */
 	public Login() {
-
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		
+		setResizable(false);
 
 		
 		setTitle("Login");
+		
+		setLocationRelativeTo(null);
 
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 521, 291);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
+		// UTILIZADO PARA CENTRAR EL GUI
+		setLocationRelativeTo(null);
+		
 		JLabel lblUsuario = new JLabel("Usuario");
-		lblUsuario.setBounds(72, 83, 97, 14);
+		lblUsuario.setFont(new Font("Georgia", Font.PLAIN, 13));
+		lblUsuario.setBounds(21, 62, 97, 19);
 		contentPanel.add(lblUsuario);
 		
 		txtUsuario = new JTextField();
-		txtUsuario.setBounds(179, 78, 86, 20);
+		txtUsuario.setBounds(139, 62, 131, 20);
 		contentPanel.add(txtUsuario);
 		txtUsuario.setColumns(10);
 		
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
-		lblContrasea.setBounds(72, 114, 97, 14);
+		lblContrasea.setFont(new Font("Georgia", Font.PLAIN, 13));
+		lblContrasea.setBounds(21, 107, 97, 19);
 		contentPanel.add(lblContrasea);
 		
 		txtContraseña = new JPasswordField();
-		txtContraseña.setBounds(179, 109, 86, 17);
+		txtContraseña.setBounds(139, 107, 131, 19);
 		contentPanel.add(txtContraseña);
 		
 		JComboBox cboElije = new JComboBox();
 		cboElije.setModel(new DefaultComboBoxModel(new String[] {"Cliente", "Empresa"}));
 		cboElije.setSelectedIndex(-1);
-		cboElije.setBounds(72, 150, 86, 20);
+		cboElije.setBounds(139, 154, 131, 20);
 		contentPanel.add(cboElije);
 		
-		JLabel lblNewLabel = new JLabel("Admin");
+		JLabel lblNewLabel = new JLabel("admin..");
+		lblNewLabel.setForeground(new Color(0, 102, 204));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Georgia", Font.PLAIN, 13));
 		lblNewLabel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
+				
+				Administrador a = new Administrador();
+				a.setVisible(true);
+				dispose();
+				
 			}
 		});
 
-		lblNewLabel.setBounds(12, 209, 86, 14);
+		lblNewLabel.setBounds(10, 214, 86, 20);
 		contentPanel.add(lblNewLabel);
 		
-		JLabel lblTimer = new JLabel("La ventana se cerrarÃ¡ en");
-		lblTimer.setBounds(100, 19, 208, 14);
+		JLabel lblTimer = new JLabel("La ventana se cerrar\u00E1 en : ");
+		lblTimer.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTimer.setFont(new Font("Georgia", Font.PLAIN, 13));
+		lblTimer.setBounds(98, 19, 205, 22);
 		contentPanel.add(lblTimer);
 		
-		lblCounter = new JLabel("10");
-		lblCounter.setBounds(274, 19, 31, 14);
+		lblCounter = new JLabel("");
+		lblCounter.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCounter.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblCounter.setBounds(278, 19, 50, 22);
 		contentPanel.add(lblCounter);
 		
 		JLabel lblNewLabel_1 = new JLabel("Imagen");
+		lblNewLabel_1.setIcon(new ImageIcon(Login.class.getResource("/images/imagenes/Llave.jpg")));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(245, 45, 179, 136);
+		lblNewLabel_1.setBounds(292, 49, 179, 136);
 		contentPanel.add(lblNewLabel_1);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton btnIngresar = new JButton("Ingresar");
-
-
-				btnIngresar.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						
-						int usuario = 0;
-						String cod = null, password = null;
-						
-						int tipoUsuario=cboElije.getSelectedIndex();
-						String userInput = txtUsuario.getText();
-						password = txtContraseña.getText();
-						
-						if (Validation.isNumeric(userInput)) {
-							usuario = Integer.parseInt(userInput);
-						} else {
-							cod = userInput;
-						}
-						
-						try {
-							switch (tipoUsuario) {
-								case 0:
-									ClientModel cli = new ClientModel();
-									cliente = usuario == 0 ? cli.getClient(cod) : cli.getClient(usuario);
-									System.out.println(cliente);
-									openMenu(cliente, cod != null ? cod : usuario, tipoUsuario, password);
-									break;
-								case 1:
-									EnterpriseModel em = new EnterpriseModel();
-									empresa = usuario == 0 ? em.getEnterprise(cod) : em.getEnterprise(usuario + "");
-									openMenu(empresa, cod != null ? cod : usuario + "", tipoUsuario, password);
-									break;
-								default:
-									JOptionPane.showMessageDialog(null, "Ingrese un tipo de usuario", "ERROR", JOptionPane.ERROR_MESSAGE);
-									break;
-							}
-						} catch (Exception e) {
-							System.out.println(e.getMessage());
-							JOptionPane.showMessageDialog(null, "Ingrese sus Datos Correctamente", "ERROR", JOptionPane.ERROR_MESSAGE);
-						}
-					}
-				});
-
-				btnIngresar.setActionCommand("OK");
-				buttonPane.add(btnIngresar);
-				getRootPane().setDefaultButton(btnIngresar);
+		
+		JButton button = new JButton("Ingresar");
+		button.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		button.setIcon(new ImageIcon(Login.class.getResource("/images/iconos22x22/dialog-accept.png")));
+		button.setActionCommand("OK");
+		button.setBounds(214, 208, 114, 29);
+		contentPanel.add(button);
+		
+		JButton button_1 = new JButton("Registrar");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 			}
-			{
-				JButton btnRegistra = new JButton("Registrar");
-				btnRegistra.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-
-						int num=cboElije.getSelectedIndex();
-						
-						if (num == 1) {
-							RegistroEmpresa rgempre = new RegistroEmpresa();
-							rgempre.setVisible(true);
-						} else if( num == 0){
-							RegistroCliente rgcli = new RegistroCliente();
-							rgcli.setVisible(true);
-						}
-						else
-							JOptionPane.showMessageDialog(null,"Seleccione que tipo de Usuario es: ");				
-
-					}
-				});
-				btnRegistra.setActionCommand("Cancel");
-				buttonPane.add(btnRegistra);
-			}
-		}
+		});
+		button_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		button_1.setIcon(new ImageIcon(Login.class.getResource("/images/iconos22x22/user-new-3.png")));
+		button_1.setActionCommand("Cancel");
+		button_1.setBounds(346, 208, 114, 29);
+		contentPanel.add(button_1);
+		
+		JLabel lblTipoDeUsuario = new JLabel("Tipo de Usuario :");
+		lblTipoDeUsuario.setFont(new Font("Georgia", Font.PLAIN, 13));
+		lblTipoDeUsuario.setBounds(21, 154, 132, 19);
+		contentPanel.add(lblTipoDeUsuario);
 		
 	}
 	
@@ -211,7 +191,7 @@ public class Login extends JDialog {
 		else JOptionPane.showMessageDialog(null,"No existe ni empresa, Ni un usuario");
 	}
 	
-	static void initThread() {
+	public static void initThread() {
 		Timer counter = new Timer();
 		counter.start();
 		try {
@@ -219,7 +199,7 @@ public class Login extends JDialog {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
-
 }
